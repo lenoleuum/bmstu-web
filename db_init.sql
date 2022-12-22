56292459c840b6f32cabd10549c7003c24996b71
Lena <76661573+lenoleuum@users.noreply.github.com>
2022-12-22 15:11:57 +0300

    Update db_init.sql

--- db_init.sql
+++ db_init.sql
@@ -1,6 +1,8 @@
 \c mbti_db
 
-create table Users
+create schema MBTI;
+
+create table MBTI.Users
 (
 	UserUK int primary key,
 	Login varchar(100) not null ,
@@ -12,14 +14,14 @@ create table Users
 	DateOfBirth date
 );
 
-create table Types
+create table MBTI.Types
 (
 	TypeUK int primary key,
 	TypeName varchar(255) not null ,
 	TypeDescription text
 );
 
-create table Characters
+create table MBTI.Characters
 (	
 	CharacterUK int primary key,
 	CharacterName varchar(255) not null,
@@ -27,4 +29,4 @@ create table Characters
 	Category varchar(100)
 );
 
-alter table Characters add constraint Characters_TypeUk_FK foreign key (TypeUK) references Types(TypeUK);
+alter table MBTI.Characters add constraint Characters_TypeUk_FK foreign key (TypeUK) references MBTI.Types(TypeUK);
